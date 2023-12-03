4b3944e8a6fa0318172a95a66dbb307a5187f0c9
Romashka641 <145899809+Romashka641@users.noreply.github.com>
2023-12-03 21:35:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+var FirstName = document.getElementByID("first-name")
+var SecondName = document.getElementByID("second-name")
+var ThirdName = documnet.getElementByID("third-name")
+var FourthName = document.geyElementByID("fourth-name")
+var FiverName = document.getElementByID("fiver-name")
+
 
